ab81b906f4a8f9d645a1544b8d731ec31509e365
mpdcfiuba <mcostantini@fi.uba.ar>
2025-12-31 22:32:11 -0300

    hibrido vscode CMake debug en STMCubeIDE v2.0

---
+++ pipe_organ_computer.elf/cortex-m-debug/subdir.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ELF_SRCS += \
+C:/Users/tinch/Documents/Todo/Facultad/UBA/2025/2C/Embebidos/Practica/TPF\ -\ Organo\ de\ tubos/pipe_organ_computer/build/Debug/pipe_organ_computer.elf 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
